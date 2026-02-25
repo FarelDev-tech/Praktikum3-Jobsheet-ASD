0130f28c68f5954abe5ff909e00f06fa8cd14111
fareldev <ffarelscho@gmail.com>
2026-02-25 09:03:16 +0700

    Modification to answer question no1 percobaan 2

--- mahasiswa08.java
+++ mahasiswa08.java
@@ -3,4 +3,12 @@ public class mahasiswa08 {
     public String nama;
     public String kelas;
     public float ipk;
+
+    // method cetakInfo tuk menjawab pertanyaan 1
+    public void cetakInfo() {
+        System.out.println("NIM: " + nim);
+        System.out.println("Nama: " + nama);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
 }
